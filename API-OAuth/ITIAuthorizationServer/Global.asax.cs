cbf875a50a779ef7174d65182f5ccc6faff03698
Alameer Ashraf Hassan <alamiir.ashraf@gmail.com>
2017-06-05 09:03:33 +0200

    Remove unnecssary views

--- API-OAuth/ITIAuthorizationServer/Global.asax.cs
+++ API-OAuth/ITIAuthorizationServer/Global.asax.cs
@@ -16,7 +16,7 @@ namespace ITIAuthorizationServer
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
             AutoMapperConfiguration.Configure();
 
         }
