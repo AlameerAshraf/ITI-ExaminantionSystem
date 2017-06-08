c99f2f68aadb09eac5f89d4b1721e137ac3eb196
Alameer Ashraf Hassan <alamiir.ashraf@gmail.com>
2017-06-08 11:21:56 +0200

    Send Mail | SMTP

--- Application-Main/App-iti/App-iti/Controllers/EmployeeController.cs
+++ Application-Main/App-iti/App-iti/Controllers/EmployeeController.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.SignalR.Client;
+using System.Net.Mail;
 
 namespace App_iti.Controllers
 {
@@ -56,12 +57,42 @@ namespace App_iti.Controllers
             var ExcpectedObj = new { Email = "" };
             var EmpEmailState = JsonConvert.DeserializeAnonymousType(responseData, ExcpectedObj);
 
-
-
+            if (EmpEmailState.Email != "0")
+            {
+                SendMailSMTP(EmpEmailState.Email, "20/20/1994");
+            }
+            else
+                return Content("Done");
             return Content(EmpEmailState.ToString());
 
         }
 
+
+
+        public void SendMailSMTP(string External_Mail , string Exam_Date)
+        {
+            string SendTo = External_Mail;
+            string Subject = "Enter Exam | ITI MS";
+            string Message = "Hi Instructor , Your course exam have been scheduled at " + Exam_Date + " So Please add your Exam !";
+
+            MailMessage Mess = new MailMessage();
+            Mess.From = new MailAddress("alameerelnagar94@gmail.com");
+            Mess.To.Add(External_Mail);
+            Mess.Subject = Subject;
+            Mess.Body = Message;
+            Mess.IsBodyHtml = true;
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            System.Net.NetworkCredential NetworkCardential = new System.Net.NetworkCredential();
+            NetworkCardential.UserName = "alameerelnagar94@gmail.com";
+            NetworkCardential.Password = "01060931989";
+            smtp.UseDefaultCredentials = true;
+            smtp.Credentials = NetworkCardential;
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+            smtp.Send(Mess);
+        }
 
         public ActionResult SignalREmp1()
         {
